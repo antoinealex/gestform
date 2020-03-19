a4c1993997ea473d8c7338b9ea519abad02967b0
antoine@antoinealexandre.eu <ComLigue1924>
2020-03-19 11:11:02 +0100

    - Added Get Api access methods in CalendarController.php - Added findOneById in CalendarEventRepository.php

--- src/Repository/CalendarEventRepository.php
+++ src/Repository/CalendarEventRepository.php
@@ -19,22 +19,36 @@ class CalendarEventRepository extends ServiceEntityRepository
         parent::__construct($registry, CalendarEvent::class);
     }
 
-    // /**
-    //  * @return CalendarEvent[] Returns an array of CalendarEvent objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param Int $userID
+     * @return CalendarEvent[] Returns an array of CalendarEvent objects
+     */
+    public function findByUserID(Int $userID)
     {
         return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
+            ->andWhere('c.user = :val')
+            ->setParameter('val', $userID)
             ->orderBy('c.id', 'ASC')
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+
+    /**
+     * @param Int $id
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findOneByID(Int $id): ?CalendarEvent
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.id = :val')
+            ->setParameter('val', $id)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
 
     /*
     public function findOneBySomeField($value): ?CalendarEvent
