22480a8e1110cf7d6c50378210259d01a7fd1235
antoine@antoinealexandre.eu <ComLigue1924>
2020-03-23 15:13:04 +0100

    - All methods are written in CalendarController.php - Tested with Postman, see more here : https://documenter.getpostman.com/view/10535977/SzS8tkdv

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use DateTime;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManager;
 
 use App\Entity\CalendarEvent;
 use App\Entity\User;
@@ -65,10 +68,14 @@ class CalendarController extends AbstractController
         return $response;
 	}
 
-	/**
-	 * @Route("/event", name="event", methods={"GET"})
-	 */
-	public function getEventById(Request $request) : Response
+    /**
+     * @Route("/event", name="event", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+	public function getEventById(Request $request
+    ) : Response
 	{
         $eventId = $request->query->get('id');
         $event =  $this->getDoctrine()->getRepository(CalendarEvent::class)->findOneByID($eventId);
@@ -93,20 +100,202 @@ class CalendarController extends AbstractController
 	/* ----POST---*/
 	/* -----------*/
 
-	/**
-	 * @Route("/new_user_event", name="new_user_ev", methods={"POST"})
-	 */
-	public function newUserEvent() : Response
+    /**
+     * @Route("/new_user_event", name="new_user_ev", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws \Exception
+     */
+	public function newUserEvent(Request $request) : Response
 	{
+	    //Get data from POST request
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneById($request->request->get("userID"));
+        $startEvent = $request->request->get("startEvent");
+        $endEvent = $request->request->get("endEvent");
+        $status = $request->request->get("status");
+        $description = $request->request->get("eventDescription");
 
+        //Creation of objects
+        $em = $this->getDoctrine()->getManagerForClass(CalendarEvent::class);
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $event = new CalendarEvent();
+
+        try {
+            //Hydrate new event object
+            $event->setuser($user);
+            $event->setStartEvent(new DateTime($startEvent));
+            $event->setEndEvent(new DateTime($endEvent));
+            $event->setStatus($status);
+            $event->setEventDescription($description);
+        }
+        catch (\Exception $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+            return $response;
+        }
+
+        //Persistence
+        try {
+            $em->persist($event);
+            $em->flush();
+        }
+        catch (\Exception $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+            return $response;
+        }
+
+        //Return
+        $response->setContent(json_encode(["success" => TRUE]));
+        return $response;
 	}
 
-	/**
-	 * @Route("/new_user_appointment", name="new_user_apt", methods={"POST"})
-	 */
-	public function newUserAppointment() : Response
+    /**
+     * @Route("/new_user_appointment", name="new_user_apt", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+	public function newUserAppointment(Request $request) : Response
 	{
+        //Get data from POST request
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneById($request->request->get("userID"));
+        $startEvent = $request->request->get("startEvent");
+        $endEvent = $request->request->get("endEvent");
+        $status = $request->request->get("status");
+        $description = $request->request->get("eventDescription");
+        $invitation = $this->getDoctrine()->getRepository(User::class)->findOneById($request->request->get("idUserInvitation"));
 
+        //Creation of objects
+        $em = $this->getDoctrine()->getManagerForClass(CalendarEvent::class);
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $event = new CalendarEvent();
+
+        try {
+            //Hydrate new event object
+            $event->setuser($user);
+            $event->setStartEvent(new DateTime($startEvent));
+            $event->setEndEvent(new DateTime($endEvent));
+            $event->setStatus($status);
+            $event->setEventDescription($description);
+            $event->setuserInvited($invitation);
+        }
+        catch (\Exception $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+            return $response;
+        }
+
+        //Persistence
+        try {
+            $em->persist($event);
+            $em->flush();
+        }
+        catch (\Exception $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+            return $response;
+        }
+
+        //Return
+        $response->setContent(json_encode(["success" => TRUE]));
+        return $response;
 	}
 
+    /* -----------*/
+    /* ----PUT----*/
+    /* -----------*/
+
+    /**
+     * @Route("/update_event", name="update_event", methods={"PUT"})
+     * @param Request $request
+     * @return Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function updateEvent(Request $request): Response
+    {
+        //Get and decode Data from request body
+        $requestParams = $request->getContent();
+        $content = json_decode($requestParams, TRUE);
+
+        //Fetch Data in local variables
+        $eventId= $content["eventId"];
+        $startEvent = $content["startEvent"];
+        $endEvent = $content["endEvent"];
+        $status = $content["status"];
+        $description = $content["eventDescription"];
+        if (isset($content["idUserInvitation"])) {
+            $invitation = $this->getDoctrine()->getRepository(User::class)->findOneById($content["idUserInvitation"]);
+        } else {
+            $invitation = NULL;
+        }
+
+        //Get the event from DBAL
+        $event = $this->getDoctrine()->getRepository(CalendarEvent::class)->findOneByID($eventId);
+
+        //Get Entity Manager
+        $em = $this->getDoctrine()->getManagerForClass(CalendarEvent::class);
+
+        //Prepare HTTP Response
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        //Update event object
+        try {
+            $event->setStartEvent(new DateTime($startEvent));
+            $event->setEndEvent(new DateTime($endEvent));
+            $event->setStatus($status);
+            $event->setEventDescription($description);
+            $event->setuserInvited($invitation);
+        }
+        catch (\Exception $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+        }
+
+        //Persistence
+        try {
+            $em->persist($event);
+            $em->flush();
+            $response->setContent(json_encode(["success" => TRUE]));
+        }
+        catch (\Exception $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+        }
+        return $response;
+    }
+
+    /* ------------*/
+    /* ---DELETE---*/
+    /* ------------*/
+
+    /**
+     * @Route("/delete_event", name="delete_event", methods={"DELETE"})
+     * @param Request $request
+     * @return Response
+     */
+    public function deleteEvent(Request $request): Response
+    {
+        //Get Entity Manager and prepare response
+        $em = $this->getDoctrine()->getManagerForClass(CalendarEvent::class);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        //Get event object to to delete
+        $eventId = $request->query->get("eventId");
+        try {
+            $event = $em->getRepository(CalendarEvent::class)->findOneByID($eventId);
+        } catch (NonUniqueResultException $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+        }
+
+        //Remove object
+        try {
+            $em->remove($event);
+            $em->flush();
+            $response->setContent(json_encode(["success" => TRUE]));
+        } catch (\Exception $e) {
+            $response->setContent(json_encode(["success" => FALSE]));
+        }
+        return $response;
+    }
 }
