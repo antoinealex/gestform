ef8c38a258d2b1f57e003fb0cd644f5c2172cc66
antoine@antoinealexandre.eu <ComLigue1924>
2020-04-06 10:42:58 +0200

    Revert changes in :              - bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -7,10 +7,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-<<<<<<< HEAD
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-=======
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
->>>>>>> fae7d782e28abb261193fdaa3809071200568d54
 ];
